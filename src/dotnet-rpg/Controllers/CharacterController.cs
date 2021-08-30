e769512d853af80e937aaa0d409165577d6ae570
Azibataram Clement <clementazibataram@gmail.com>
2021-08-30 22:35:58 +0100

    add first endpoint

---
+++ src/dotnet-rpg/Controllers/CharacterController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using dotnet_rpg.Models;
+
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace dotnet_rpg.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class CharacterController : ControllerBase
+    {
+        private static Character Knight = new Character();
+
+        public IActionResult Get()
+        {
+            return Ok(Knight);
+        }
+    }
+}
---
+++ src/dotnet-rpg/Models/Character.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dotnet_rpg.Models
+{
+    public class Character
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = "Frodo";
+        public int HitPoints { get; set; } = 10;
+        public int Strength { get; set; } = 10;
+        public int Defence { get; set; } = 10;
+        public int Intelligence { get; set; } = 10;
+    }
+}
